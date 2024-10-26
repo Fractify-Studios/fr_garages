66c39f7ee6abcde88bd90103c19bcadbfc70f2f9
feenko <147188774+feenko@users.noreply.github.com>
2024-10-26 19:56:04 +0200

    feat: improve sql performance

--- base.sql
+++ base.sql
@@ -1,12 +1,15 @@
 DROP TABLE IF EXISTS owned_vehicles;
 CREATE TABLE owned_vehicles (
-    owner VARCHAR(255) NOT NULL,
-    second_owner VARCHAR(255) DEFAULT NULL,
-    plate VARCHAR(255) NOT NULL PRIMARY KEY,
-    model VARCHAR(255) NOT NULL,
+    owner VARCHAR(128) NOT NULL,
+    second_owner VARCHAR(128) DEFAULT NULL,
+    plate VARCHAR(64) NOT NULL PRIMARY KEY,
+    model VARCHAR(128) NOT NULL,
     properties TEXT NOT NULL,
-    type VARCHAR(255) DEFAULT 'car',
+    type VARCHAR(128) DEFAULT 'car',
     `stored` BOOLEAN DEFAULT FALSE,
-    parking VARCHAR(255) DEFAULT NULL,
-    slot INT DEFAULT NULL
+    parking VARCHAR(64) DEFAULT NULL,
+    slot SMALLINT UNSIGNED DEFAULT NULL,
+    INDEX (owner),
+    INDEX (second_owner),
+    INDEX (plate)
 );
